cdf075612eaef7ca921e884ec1b84873e9b226d4
pegahkeshavarz <johndoe@example.com>
2016-06-23 13:16:34 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var path = require("path");
+// create the express app
+var app = express();
+// require bodyParser since we need to handle post data for adding a user
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+// static content
+app.use(express.static(path.join(__dirname, "./client/static")));
+
+
+require('./server/config/mongoose.js');
+
+ var routes_setter = require('./server/config/routes.js');
+
+ routes_setter(app);
+
+app.listen(8001, function() {
+  console.log("listening on port 8000");
+})
