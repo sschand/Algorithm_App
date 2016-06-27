db6b10039f14b826081b16b0e098814ade83819b
pegahkeshavarz <johndoe@example.com>
2016-06-27 13:22:26 -0700

    angular added

--- server/models/algorithm.js
+++ server/models/algorithm.js
@@ -1,2 +1,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+var ChatSchema = new mongoose.Schema({
+  chats: [{type: String, required:true}],
+  chat_id: {type: Schema.Types.ObjectId, ref: "Chat"},
+  algorithm_id: {type: Schema.Types.ObjectId, ref: "Algorithm"}
+});
+
+mongoose.model('Chat', ChatSchema);
