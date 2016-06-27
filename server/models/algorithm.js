e165b79ce1f0470b0f22f9a6ac82ed61f304c79d
Sharol Chand <sharolchand@gmail.com>
2016-06-27 13:25:36 -0700

    adding Algorithm model

--- server/models/algorithm.js
+++ server/models/algorithm.js
@@ -7,13 +7,6 @@ var algorithmSchema = new mongoose.Schema({
     users: [{type: mongoose.Schema.Types.ObjectId, ref: "Users"}],
     chat_id: {type: mongoose.Schema.Types.ObjectId, ref: "Chats"},
     category: {type: String}
-    // name: {type: String},
-    // posts: [{type: mongoose.Schema.Types.Mixed, ref: "Posts"}],
-    // _topics: [{type: mongoose.Schema.Types.Mixed, ref: "Topics"}],
-    // _comments: [{type: mongoose.Schema.Types.Mixed, ref: "Comments"}],
-    // postPosted: Number,
-    // topicsPosted: Number,
-    // commentsPosted: Number
 }, {timestamps: true})
 
 // register schema as a model
