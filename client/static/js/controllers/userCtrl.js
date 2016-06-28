ba2821649f40ff5d3d03db08e9fffb8544be8af9
Sharol Chand <sharolchand@gmail.com>
2016-06-28 10:52:16 -0700

    begin login stuff

--- client/static/js/controllers/userCtrl.js
+++ client/static/js/controllers/userCtrl.js
@@ -1,25 +1,31 @@
 algorithmApp.controller('userCtrl', function ($scope, userFactory, $location) {
     $scope.current_user;
     $scope.password_error = '';
+    $scope.login_error = '';
 
     // Register User
     $scope.registerUser = function(){
-        // console.log('info', $scope.newUser);
-
         if($scope.newUser.password !== $scope.newUser.c_password){
             $scope.password_error = "Passwords don't match.";
         }else{
             $scope.password_error = '';
 
             userFactory.create($scope.newUser, function(data){
-                $scope.current_user = data;
+                // $scope.current_user = data;
                 $('#register').closeModal();
                 alert('You have been registered, please log in to continue');
-
-                // $location.url();
             });
-
-            // reset user register form $scope.newUser = {};
         }
+    }
+
+    // Login user
+    $scope.loginUser = function(){
+        userFactory.loginUser($scope.login, function(data){
+            // $scope.current_user = data;
+            
+            // set user info in factory
+            // Take to dashboard page
+
+        });
     }
 });
